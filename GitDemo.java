fb3c84555f4b42a6b1ec6f71c90462f5c6cedf91
kushagrakshatri <38435478+kushagrakshatri@users.noreply.github.com>
2021-02-25 18:41:47 -0700

    Add name and edit addNumber()
    Changes Made: 
    1. Changed line 2 to include my name.
    2. addNum() calculates the sum of num and previousNumber.
    3. Added private double reverseNum variable to the GitDemo class.
    4. Added the statement 'reverseNum = newNumber' in the reverseNumber() function.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Kushagra Kshatri
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        int sum = num + previousNumber;
+        return sum;
     }
     
     /*****
@@ -41,6 +43,8 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         double newNumber = 1;
+        
+        reverseNum = newNumber;
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
